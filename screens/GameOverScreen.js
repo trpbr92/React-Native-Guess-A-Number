f0a2690adff4c0b056729845d906e11286f5dad0
Trace Brandt <trpbr92@gmail.com>
2021-07-27 10:05:32 -0500

    image styling

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -4,8 +4,10 @@ import { View, Text, StyleSheet, Button, Image } from 'react-native';
 const GameOverScreen = props => {
     return (
         <View style={styles.screen}>
-           <Text>Game Over!</Text> 
+           <Text>Game Over</Text> 
+           <View style={styles.imageContainer}>
            <Image source={require('../assets/success.png')} style={styles.image} />
+           </View>
            <Text>Number of rounds: {props.roundsNumber}</Text>
            <Text>Number was: {props.userNumber}</Text> 
            <Button title ="NEW GAME" onPress={props.onRestart} />
@@ -20,10 +22,19 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center'
     },
+    imageContainer: {
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: 'black',
+        overflow: 'hidden',
+        marginVertical: 30
+    },
     image: {
-        width: '80%',
-        height: 300
-    }
+        width: '100%',
+        height: '100%',
+    },
 })
 
 export default GameOverScreen;
